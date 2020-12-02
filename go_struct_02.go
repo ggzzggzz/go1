1afb1d27826039819a5d9e45a2821b2846bd87f0
ggzzggzz <ggzz553@gmail.com>
2020-12-02 17:52:44 +0900

    v2 struct

--- go_struct_02.go
+++ go_struct_02.go
@@ -20,6 +20,9 @@ func defaultSubscriber(name string) subscriber {
 	s.active = true
 	return s
 }
+func applyDiscount(s *subscriber) {
+	s.rate = 4000
+}
 func main() {
 	s1 := defaultSubscriber("kim")
 	s1.rate = 4500
@@ -27,4 +30,8 @@ func main() {
 
 	s2 := defaultSubscriber("park")
 	printInfo(s2)
+
+	var s3 subscriber
+	applyDiscount(&s3)
+	fmt.Println(s3.rate)
 }
